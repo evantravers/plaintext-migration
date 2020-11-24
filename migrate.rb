6cba3c7d14972c3c1b7a3f030e444b207d98022a
Evan Travers <evantravers@gmail.com>
2020-11-24 16:08:49 -0600

    Refactoring
    NO clue if this is better really... elixir has ruined me as a rubyist.

--- migrate.rb
+++ migrate.rb
@@ -1,107 +1,103 @@
 require './lib/zettel'
 require 'set'
 
-unique_ids = Set.new
-DST = '../../wiki/'
-
-def file_crawl(src)
-  Dir.glob("#{src}**/*.{md,txt}") do |file|
-    yield file
-  end
-end
-
-def set_metadata(zettel, string)
-  string
-    .split(/\n/)
-    .map{|metadata|
-      key, value = metadata.split(': ')
-      if key == 'tags' then
-        key = 'keywords'
-        value = value.split(', ')
-      end
-    }.each {|k, v|
-      binding.pry
-      puts k
-      puts v
-      zettel.set(k, v)
-    }
-end
-
 # Writing some tests
 test = Zettel.new
 test.set('id', '20200998955')
-test.set('title', 'This is a test of a note')
+test.set('title', 'If you can read this...')
 test.set('keywords', ['tag1', 'tag2', '#tag3'])
 test.set('isbn', '0192u319231098')
-test.body = "Call me Ishmael..."
+test.body = "Then the tests are working."
 
 puts "<< #{test.render_filename} >>"
 puts test.render
 
-# clean out the folder
-Dir.each_child(DST){ |f| File.delete(File.join(DST, f)) unless f.match("obsidian") }
+class Migrator
+  # links = '../links/'
+  # books = '../booknotes/'
+  # diary = '../diary/'
+  # plaintext = '../notes/'
 
-# Pull in each source:
-def old_zettel(opts = {test: :true})
-  file_crawl('../zk/') do |old_zettel|
-    z = Zettel.new()
-    content = File.read(old_zettel)
+  # - new zk (seems to be split between two folders 😿 )
+  # - links
+  # - booknotes (include subfolders)
+  # - diary
+  # - notes (include subfolders)
+  #
+  # FOR EACH by folder:
+  #   pull in each file
+  #   build the Zettel object
+  #   write it to the new folder
+  #     IO.write('../../wiki/', zettel.render())
 
-    if content.match(/^\w+: \w+$/) then
-      metadata, *content = content.split(/\n\n/)
+  # Build Zettel objects
+  #   For each one, ensure that it has a unique ID compared to the other ones
 
-      if content.class == Array then
-        content = content.join("\n\n")
+  @unique_ids = Set.new
+  DST = '../../wiki/'
+
+  def self.file_crawl(src)
+    Dir.glob("#{src}**/*.{md,txt}") do |file|
+      yield file
+    end
+  end
+
+  def self.old_zettel(opts = {test: true})
+    file_crawl('../zk/') do |old_zettel|
+
+      z = Zettel.new()
+      content = File.read(old_zettel)
+
+      if content.match(/^\w+: .+$/) then
+        metadata, *content = content.split(/\n\n/)
+
+        if content.class == Array then
+          content = content.join("\n\n")
+        end
+
+        metadata
+          .split(/\n/)
+          .map{|metadata|
+            key, value = metadata.scan(/^(\w+): (.*)$/).flatten
+            if key == 'tags' then
+              key = 'keywords'
+              value = value.split(', ')
+            end
+            z.set(key, value)
+          }
+      end
+      if !z.id then
+        # try in the filename?
+        z.set('id', old_zettel.scan(/\d{12}-/)[0].gsub('-', ''))
       end
 
-      metadata
-        .split(/\n/)
-        .map{|metadata|
-          key, value = metadata.split(': ')
-          if key == 'tags' then
-            key = 'keywords'
-            value = value.split(', ')
-          end
-          z.set(key, value)
-        }
+      if !z.title then
+        title, *content = content.split(/\n/)
+        title = title.gsub(/^#+ /, '')
+        z.set(:title, title)
+        content = content.join("\n")
+      end
+
+      z.body = content
+
+      if opts[:test] then
+        puts "<< #{z.render_filename()} >>"
+        puts z.render()
+      else
+        File.write("#{DST}/#{z.render_filename()}", z.render())
+      end
     end
+  end
 
-    if !z.id then
-      # try in the filename?
-      z.set('id', old_zettel.scan(/^\d{12}-/)[0])
-    end
+  def self.run(testing=false)
+    # clean out the folder
+    Dir.each_child(DST){ |f| File.delete(File.join(DST, f)) unless f.match("obsidian") }
 
-    if !z.title then
-      title, *content = content.split(/\n/)
-      z.set(:title, title)
-      content = content.join("\n")
-    end
-
-    z.body = content
-
-    puts "<< #{z.render_filename()} >>"
-    puts z.render()
+    # Pull in each source:
+    old_zettel(test: testing)
   end
 end
 
-links = '../links/'
-books = '../booknotes/'
-diary = '../diary/'
-plaintext = '../notes/'
+Migrator.run()
 
-# - new zk (seems to be split between two folders 😿 )
-old_zettel()
-# - links
-# - booknotes (include subfolders)
-# - diary
-# - notes (include subfolders)
-#
-# FOR EACH by folder:
-#   pull in each file
-#   build the Zettel object
-#   write it to the new folder
-#     IO.write('../../wiki/', zettel.render())
-
-# Build Zettel objects
-#   For each one, ensure that it has a unique ID compared to the other ones
 
